da7aa9d4b42bc9012eb9b8c536e1a30c450887a1
pauseplayrepeat <mrsqueex@gmail.com>
2024-01-15 23:59:33 -0600

    update route

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -4,38 +4,48 @@ import prisma from '@/libs/prismadb';
 import getCurrentUser from '@/actions/getCurrentUser';
 import { auth } from '@clerk/nextjs';
 
-export async function POST(request: Request) {
-    const { userId } = auth();
+export async function POST(
+  request: Request, 
+) {
+  const currentUser = await getCurrentUser();
 
-    if (!userId) {
-        return NextResponse.error();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const body = await request.json();
+  const { 
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location,
+    price,
+   } = body;
+
+  Object.keys(body).forEach((value: any) => {
+    if (!body[value]) {
+      NextResponse.error();
     }
+  });
 
-    const body = await request.json();
-    const {
-        title,
-        description,
-        imageSrc,
-        category,
-        location,
-        price,
-        experience, // new field
-        expertise, // new field
-        sessionLength, // new field
-    } = body;
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      locationValue: location.value,
+      price: parseInt(price, 10),
+      userId: currentUser.id
+    }
+  });
 
-    const listing = await prisma.listing.create({
-        data: {
-            title,
-            description,
-            imageSrc,
-            category,
-            locationValue: location.value,
-            price: parseInt(price, 10),
-            userId,
-            experience: parseInt(experience, 10),
-            expertise, // new field
-            sessionLength: parseInt(sessionLength, 10),
-        },
-    });
+  return NextResponse.json(listing);
 }
