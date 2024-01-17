7317a6e37402374532ffbed6460ec31eccc97c1a
pauseplayrepeat <mrsqueex@gmail.com>
2024-01-16 18:12:43 -0600

    updating search modal

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -7,7 +7,7 @@ import { Range } from 'react-date-range';
 import { formatISO } from 'date-fns';
 import { useRouter, useSearchParams } from 'next/navigation';
 
-import useSearchModal from "@/hooks/useSearchModal";
+
 
 import Modal from "./Modal";
 import Calendar from "../inputs/Calendar";
@@ -16,6 +16,7 @@ import CountrySelect, {
   CountrySelectValue
 } from "../inputs/CountrySelect";
 import Heading from '../Heading';
+import useSearchModal from '@/hooks/useSearchModal';
 
 enum STEPS {
   LOCATION = 0,
